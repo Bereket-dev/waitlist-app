747d0f08e6dce2e03b4edd772241cd1fb79c4cd0
Bereket <bereketdesalegn097@gmail.com>
2025-08-22 03:31:50 +0300

    initial firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,29 @@
+// services/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Analytics
+const analytics = getAnalytics(app);
+
+// Firestore
+const db = getFirestore(app);
+
+// Auth (needed if you use Firebase Extensions for email)
+const auth = getAuth(app);
+
+export { app, analytics, db, auth };
