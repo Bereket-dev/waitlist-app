82a692ce66eafcc4e98a8736e7277daaf507fd00
Bereket <bereketdesalegn097@gmail.com>
2025-08-22 22:30:06 +0300

    enhance reposiceness

--- src/components/WaitlistForm.jsx
+++ src/components/WaitlistForm.jsx
@@ -37,12 +37,12 @@ function WaitlistForm() {
         required
         disabled={loading}
         placeholder="Enter your email"
-        className="w-full h-[60px] disabled:opacity-70 rounded-xl bg-[#181818] border border-white/10 px-6 py-3 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#FAF1CB]/60 transition"
+        className="w-full  md:h-[60px] disabled:opacity-70 rounded-xl bg-[#181818] border border-white/10 px-6 py-3 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#FAF1CB]/60 transition"
       />
       <button
         type="submit"
         disabled={loading}
-        className="w-full h-[60px] disabled:opacity-70 rounded-xl bg-[#FAF1CB] text-black font-semibold px-6 py-3 transition hover:bg-[#e8dfb6] active:scale-[0.98]"
+        className="w-full  md:h-[60px]  disabled:opacity-70 rounded-xl bg-[#FAF1CB] text-black font-semibold px-6 py-3 transition hover:bg-[#e8dfb6] active:scale-[0.98]"
       >
         {loading ? "Joinning..." : "Join Waitlist"}
       </button>
